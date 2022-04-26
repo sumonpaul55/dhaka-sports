0bcae7b5a1599a7ddc00c03157b88ef1312e8977
sumon paul <sumpaul100@gmail.com>
2022-04-27 00:33:34 +0600

    live post page created and some other thins updated

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,14 @@
 function togleClasss(){
     document.getElementById("toggleId").classList.toggle("toggle");
 };
+
+let baseBall = document.getElementById('baseball-btn');
+function baseball(){
+    document.getElementById('baseball').classList.add('show')
+    document.getElementById('baseball').classList.remove('hide')
+    // console.log(event.target)
+}
+// function basketball(){
+//     document.getElementById('baseball').classList.add('show')
+//     document.getElementById('baseball').classList.remove('hide')
+// }
